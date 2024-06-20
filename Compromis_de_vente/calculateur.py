c66dc49afa2c9f385dde5fcbdc6afeee121636f6
Clement Teulier <teulierclement@gmail.com>
2024-06-20 17:41:33 +0200

    maj du jeudi soir

--- Compromis_de_vente/calculateur.py
+++ Compromis_de_vente/calculateur.py
@@ -56,10 +56,9 @@ def calculer_risque(
     Returns:
         int: Risk score indicating the level of risk associated with the property purchase decision.
     """
-    # Exemple simplifié de calcul de risque basé sur des règles
+
     risque = 0
 
-    # Nettoyer et valider les entrées
     try:
         revenu_mensuel = int(revenu_mensuel)
         montant_pret = int(montant_pret)
@@ -71,7 +70,6 @@ def calculer_risque(
     except ValueError:
         return "Erreur: Veuillez entrer des valeurs numériques valides pour les montants financiers."
 
-    # Extraire la durée en années
     try:
         duree_remboursement_annees = int(
             "".join(filter(str.isdigit, duree_remboursement))
@@ -79,7 +77,6 @@ def calculer_risque(
     except ValueError:
         return "Erreur: Durée de remboursement non valide."
 
-    # Facteurs de risque basés sur les revenus et les dettes
     if revenu_mensuel < 2000:
         risque += 10
     if dettes_en_cours == "***Oui***":
@@ -87,33 +84,27 @@ def calculer_risque(
     if revenus_supplementaires == "***Non***":
         risque += 5
 
-    # Facteurs de risque basés sur le prêt et la capacité de remboursement
     if montant_pret > (revenu_mensuel * 12 * duree_remboursement_annees):
         risque += 15
 
-    # Facteurs de risque basés sur l'expérience et l'historique
     if deja_proprietaire == "***Non***":
         risque += 5
     if antecedents_credit == "***Oui***":
         risque += 5
 
-    # Facteurs de risque basés sur le type de bien et sa localisation
     if type_bien == "***Ancien***":
         risque += 5
 
-    # Facteurs de risque basés sur la capacité d'épargne et l'apport initial
     if epargne_mensuelle < 500:
         risque += 5
     if capital_disponible < (0.2 * prix_achat):
         risque += 10
 
-    # Facteurs de risque basés sur les coûts supplémentaires
     if couts_renovation > 0:
         risque += 5
     if taxes_foncieres > 1000:
         risque += 5
 
-    # Ajuster le risque selon la durée de séjour prévue
     if rester_longtemps == "***Non***" or projets_revente == "***Oui***":
         risque += 5
 
--- Compromis_de_vente/mixtral_chain.py
+++ Compromis_de_vente/mixtral_chain.py
@@ -1,6 +1,5 @@
 from langchain.text_splitter import (
     CharacterTextSplitter,
-    RecursiveCharacterTextSplitter,
 )
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
@@ -21,21 +20,13 @@ def create_retriever(pdf_loaded):
     Returns:
         ChromaRetriever: A retriever object for performing semantic searches on the document embeddings.
     """
-    text_splitter = CharacterTextSplitter(
-        chunk_size=2000, chunk_overlap=250
-    )  # Le paramètre chunk_overlap détermine le nombre de caractères qui se chevauchent entre deux segments de texte consécutifs lors du découpage du texte en morceaux (chunks).
+    text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=250)
     texts = text_splitter.split_documents(pdf_loaded)
 
-    # We will use HuggingFace embeddings
     embeddings = HuggingFaceEmbeddings()
 
-    # Using Chroma vector database to store and retrieve embeddings of our text
-    db = Chroma.from_documents(
-        texts, embeddings
-    )  # Creation de la base de donné vectorielle chroma
-    retriever = db.as_retriever(
-        search_kwargs={"k": 20}
-    )  # retriever sert à faire des recherches sémantiques.
+    db = Chroma.from_documents(texts, embeddings)
+    retriever = db.as_retriever(search_kwargs={"k": 20})
 
     time.sleep(2)
     return retriever
@@ -55,12 +46,6 @@ def question_answer(query, qa_chain):
         str: The answer to the query.
     """
     chat_history = []
-    # while True:
-    # query = input("Prompt: ")
-    # To exit: use 'exit', 'quit', 'q', or Ctrl-D.",
-    # if query.lower() in ["exit", "quit", "q"]:
-    # print("Exiting")
-    # sys.exit()
     result = qa_chain({"question": query, "chat_history": chat_history})
     print("Answer: " + result["answer"] + "\n")
 
@@ -94,29 +79,7 @@ def summarize(qa_chain):
 
     Tu rempliras chaque section avec les informations adéquates en organisant ton compte-rendu en bullet points."""
 
-    # prompt = f"- Hypothèque et servitudes"
-
     chat_history = []
-
     result = qa_chain({"question": prompt, "chat_history": chat_history})
-
     time.sleep(2)
     return result["answer"]
-
-
-# def summarize(qa_chain):
-#     prompt = f"""
-#         Je souhaite que tu me rédiges un résumé du document qui sera organisé selon les points suivants:
-
-#         - section "Diagnostics de Performance Energetique (DPE)" (en parlant seulement des classes)
-
-#     ,
-
-#     Tu rempliras chaque section avec les informations adéquates en organisant ton compte-rendu en bullet points."""
-
-#     # prompt = f"- Hypothèque et servitudes"
-
-#     chat_history = []
-
-#     result = qa_chain({"question": prompt, "chat_history": chat_history})
-#     return result["answer"]
