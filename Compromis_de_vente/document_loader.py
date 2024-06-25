0ebad553301998126c9bdcfdf2c026e30454e758
Clement Teulier <teulierclement@gmail.com>
2024-06-25 15:04:13 +0200

    Classes updated

--- Compromis_de_vente/document_loader.py
+++ Compromis_de_vente/document_loader.py
@@ -4,45 +4,44 @@ from typing import Optional, List
 from langchain.schema import Document
 from tempfile import NamedTemporaryFile
 import streamlit as st
-import time
 
 
-def upload_file() -> Optional[str]:
-    """Upload and temporarily save a PDF file.
+class DocLoader:
+    def __init__(self):
+        self.pdf_docs = None
 
-    This function allows users to upload multiple PDF files through a file uploader widget.
-    It saves the first uploaded PDF to a temporary file and returns the path to the temporary file.
-    If no files are uploaded, it returns None.
+    def upload_file(self) -> Optional[str]:
+        """Upload and temporarily save a PDF file.
 
-    Returns:
-        Optional[str]: The path to the temporarily saved PDF file, or None if no files are uploaded.
-    """
-    pdf_docs: Optional[list[UploadedFile]] = st.file_uploader(
-        "Téléchargez vos PDFs ici et cliquez sur 'Process'",
-        accept_multiple_files=True,
-    )
-    if not pdf_docs:
-        return None
-    with NamedTemporaryFile(delete=False) as temp_file:
-        temp_file.write(pdf_docs[0].read())
-    return temp_file.name
+        This function allows users to upload multiple PDF files through a file uploader widget.
+        It saves the first uploaded PDF to a temporary file and returns the path to the temporary file.
+        If no files are uploaded, it returns None.
 
+        Returns:
+            Optional[str]: The path to the temporarily saved PDF file, or None if no files are uploaded.
+        """
+        self.pdf_docs = st.file_uploader(
+            "Téléchargez vos PDFs ici et cliquez sur 'Process'",
+            accept_multiple_files=True,
+        )
+        if not self.pdf_docs:
+            return None
+        with NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(self.pdf_docs[0].read())
+        return temp_file.name
 
-def load_doc(doc_link) -> List[Document]:
-    """Load and split a PDF document into smaller chunks.
+    def load_doc(self, doc_link) -> List[Document]:
+        """Load and split a PDF document into smaller chunks.
 
-    This function loads a PDF document from a given file path or link and splits it into smaller
-    chunks for easier processing.
+        This function loads a PDF document from a given file path or link and splits it into smaller
+        chunks for easier processing.
 
-    Args:
-        doc_link (str): The file path or link to the PDF document.
+        Args:
+            doc_link (str): The file path or link to the PDF document.
 
-    Returns:
-        list: A list of document chunks extracted from the PDF.
-    """
-
-    loader = PyPDFLoader(doc_link)
-    documents = loader.load()
-
-    time.sleep(2)
-    return documents
+        Returns:
+            list: A list of document chunks extracted from the PDF.
+        """
+        loader = PyPDFLoader(doc_link)
+        documents = loader.load()
+        return documents
--- Compromis_de_vente/mixtral_chain.py
+++ Compromis_de_vente/mixtral_chain.py
@@ -1,73 +1,68 @@
-from langchain.text_splitter import (
-    CharacterTextSplitter,
-)
+from langchain.text_splitter import CharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_core.vectorstores import VectorStoreRetriever
 from typing import List
-import time
 
 
-def create_retriever(pdf_loaded) -> VectorStoreRetriever:
-    """Create a retriever for semantic search from loaded PDF documents.
+class PDFtoSummary:
+    def __init__(self):
+        self.retriever = None
 
-    This function splits the text of the loaded PDF documents into chunks, generates embeddings
-    for these chunks using HuggingFace embeddings, and stores them in a Chroma vector database.
-    It then creates and returns a retriever for performing semantic searches on these embeddings.
+    def create_retriever(self, pdf_loaded) -> VectorStoreRetriever:
+        """Create a retriever for semantic search from loaded PDF documents.
 
-    Args:
-        pdf_loaded (list): A list of loaded PDF documents to be processed.
+        This function splits the text of the loaded PDF documents into chunks, generates embeddings
+        for these chunks using HuggingFace embeddings, and stores them in a Chroma vector database.
+        It then creates and returns a retriever for performing semantic searches on these embeddings.
 
-    Returns:
-        ChromaRetriever: A retriever object for performing semantic searches on the document embeddings.
-    """
-    text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=250)
-    texts = text_splitter.split_documents(pdf_loaded)
+        Args:
+            pdf_loaded (list): A list of loaded PDF documents to be processed.
 
-    embeddings = HuggingFaceEmbeddings()
+        Returns:
+            VectorStoreRetriever: A retriever object for performing semantic searches on the document embeddings.
+        """
+        text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=250)
+        texts = text_splitter.split_documents(pdf_loaded)
 
-    db = Chroma.from_documents(texts, embeddings)
-    retriever = db.as_retriever(search_kwargs={"k": 20})
+        embeddings = HuggingFaceEmbeddings()
 
-    time.sleep(2)
-    return retriever
+        db = Chroma.from_documents(texts, embeddings)
+        self.retriever = db.as_retriever(search_kwargs={"k": 20})
 
+        return self.retriever
 
-def question_answer(query, qa_chain) -> str:
-    """Get an answer to a query using a question-answering chain.
+    def question_answer(self, query, qa_chain) -> str:
+        """Get an answer to a query using a question-answering chain.
 
-    This function takes a query and a question-answering chain object, processes the query to obtain an answer,
-    and prints the answer. The chat history is maintained for context.
+        This function takes a query and a question-answering chain object, processes the query to obtain an answer,
+        and prints the answer. The chat history is maintained for context.
 
-    Args:
-        query (str): The query or question to be answered.
-        qa_chain (function): The question-answering chain function that processes the query.
+        Args:
+            query (str): The query or question to be answered.
+            qa_chain (function): The question-answering chain function that processes the query.
 
-    Returns:
-        str: The answer to the query.
-    """
-    chat_history: List = []
-    result = qa_chain({"question": query, "chat_history": chat_history})
-    print("Answer: " + result["answer"] + "\n")
+        Returns:
+            str: The answer to the query.
+        """
+        chat_history: List = []
+        result = qa_chain({"question": query, "chat_history": chat_history})
+        return result["answer"]
 
-    return result["answer"]
+    def summarize(self, qa_chain) -> str:
+        """Generate a summary of a document organized by specific sections.
 
+        This function generates a summary of a document based on a predefined template with specific sections.
+        It uses a question-answering chain to process the prompt and extract the relevant information
+        for each section. The summary is organized in bullet points.
 
-def summarize(qa_chain) -> str:
-    """Generate a summary of a document organized by specific sections.
+        Args:
+            qa_chain (function): The question-answering chain function that processes the prompt.
 
-    This function generates a summary of a document based on a predefined template with specific sections.
-    It uses a question-answering chain to process the prompt and extract the relevant information
-    for each section. The summary is organized in bullet points.
-
-    Args:
-        qa_chain (function): The question-answering chain function that processes the prompt.
-
-    Returns:
-        str: The summarized document organized by sections.
-    """
-
-    prompt = f"""
+        Returns:
+            str: The summarized document organized by sections.
+        """
+        prompt = """
         Je suis un agent immobilier et je travaille sur des compromis de vente 
         Je souhaite que tu me rédiges un résumé du document qui sera organisé selon les points suivants:
         - section "Coordonnées du Vendeur et de l'Acquéreur"
@@ -78,11 +73,10 @@ def summarize(qa_chain) -> str:
         - section "Montant à payer et Modalités de Paiement"
         - section "Date de Signature de l'Acte de Vente"
         - section "Montant de l'Indemnité d'Immobilisation et Conditions Suspensives"
-    ,
+        ,
 
-    Tu rempliras chaque section avec les informations adéquates en organisant ton compte-rendu en bullet points."""
+        Tu rempliras chaque section avec les informations adéquates en organisant ton compte-rendu en bullet points."""
 
-    chat_history: List = []
-    result = qa_chain({"question": prompt, "chat_history": chat_history})
-    time.sleep(2)
-    return result["answer"]
+        chat_history: List = []
+        result = qa_chain({"question": prompt, "chat_history": chat_history})
+        return result["answer"]
